18020e1bb3e794d6d0f3dd4a2d4c36c86a15fa1f
Jackson <jacksonwrk3@gmail.com>
2024-07-06 09:09:07 -0400

    Created isValidEmail function. Created state to handle email onchange and display email input errors

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,5 +1,39 @@
+import Link from "next/link";
+import { useState, useRef } from "react";
+import isValidEmail from "../../../../util/supabase/form-validation/isValidEmail";
+/**
+ * @description The SignUp page
+ * @returns {JSX.Element} The SignUp page
+ */
 const SignUp = () => {
-  return <div>SignUp Page</div>;
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+  /**
+   * @description Sets the email state to the email input value
+   * @param {React.ChangeEvent<HTMLInputElement>} event
+   */
+  const emailOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+
+    if (timeoutRef.current !== null) {
+      clearTimeout(timeoutRef.current);
+    } else {
+      timeoutRef.current = setTimeout(() => {
+        if (!isValidEmail(email)) {
+          setEmailError("Please enter a valid Email");
+        }
+      }, 1000);
+    }
+  };
+  return (
+    <div>
+      SignUp Page
+      <label htmlFor="email">Email</label>
+      <input id="email" type="email" value={email} onChange={emailOnChange} />
+      <Link href="/login">Already have an account? Login</Link>
+    </div>
+  );
 };
 
 export default SignUp;
---
+++ util/supabase/form-validation/isValidEmail.tsx
@@ -0,0 +1,11 @@
+/**
+ * @description Checks if the email is valid
+ * @param {string} email - The email to check
+ * @return {boolean} Whether the email is valid
+ */
+const isValidEmail = (email: string) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
+
+export default isValidEmail;
