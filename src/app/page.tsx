55d842c3924d0c0dc3d8749a24f2480f716b217b
Jackson <jacksonwrk3@gmail.com>
2024-07-05 21:13:05 -0400

    Setup/initialized supabase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,4 @@
-import Image from "next/image";
-
+import supabase from "./../../util/supabase/index";
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Landing Page
-    </main>
-  );
+  return <main>Landing Page</main>;
 }
