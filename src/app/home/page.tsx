88a8745f3037bde4c175da4cfd309e05a3948f7d
Jackson <jacksonwrk3@gmail.com>
2024-07-15 22:05:59 -0400

    Added signout feature in /home

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,21 @@
+"use client";
+import supabase from "../../../util/supabase/index";
+import { useState } from "react";
 const Home = () => {
-  return <div>Home Page</div>;
-};
+  const [signOutError, setSignOutError] = useState<null | string>(null);
 
+  const signOut = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      setSignOutError("Error signing out. Please try again");
+    }
+  };
+  return (
+    <div className="flex flex-col space-y-4">
+      Home Page
+      {signOutError && <p>{signOutError}</p>}
+      <button onClick={signOut}>Sign Out</button>
+    </div>
+  );
+};
 export default Home;
---
+++ test/supabase/signIn.test.js
@@ -0,0 +1,3 @@
+/**
+ * @TODO - Implement test for signIn
+ */
