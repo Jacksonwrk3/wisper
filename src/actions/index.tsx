2d3cf4a006c7ede0911de16900015e264ec24cbf
Jackson <jacksonwrk3@gmail.com>
2024-07-16 00:04:06 -0400

    imported signout action and exported signout action through index

--- src/actions/index.tsx
+++ src/actions/index.tsx
@@ -1,6 +1,5 @@
 import createAccount from "./createAccount.server";
 import emailTaken from "./emailTaken.server";
-import getSession from "./getSession.server";
 import signIn from "./signin.server";
 import signOut from "./signOut.server";
-export { createAccount, emailTaken, signIn, getSession, signOut };
+export { createAccount, emailTaken, signIn, signOut };
