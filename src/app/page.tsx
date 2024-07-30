fb1cb406468d9dcfdc799fecba6fe62afca9023b
Jackson <jacksonwrk3@gmail.com>
2024-07-29 22:42:20 -0400

    If user has a session  and tries to go to landing page, we redirect  to login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,27 @@
+"use client";
 import Button from "./components/Button/index.client";
 import Link from "next/link";
+import { useContext } from "react";
+import { useRouter } from "next/navigation";
+import { SessionContext } from "@/context/SessionContext.client";
 export default function LandingPage() {
-  return (
-    <main>
-      Landing Page
-      <div className="flex space-x-3 px-2">
-        <Button variant="secondary">
-          <Link href="login">Login</Link>
-        </Button>
-        <Button variant="primary">
-          <Link href="/signup">Sign Up</Link>
-        </Button>
-      </div>
-    </main>
-  );
+  const { session } = useContext(SessionContext);
+  const router = useRouter();
+  if (session !== null && session !== "Error" && typeof session === "object") {
+    router.replace("/home");
+  } else {
+    return (
+      <main>
+        Landing Page
+        <div className="flex space-x-3 px-2">
+          <Button variant="secondary">
+            <Link href="/authflow/login">Login</Link>
+          </Button>
+          <Button variant="primary">
+            <Link href="/authflow/signup">Sign Up</Link>
+          </Button>
+        </div>
+      </main>
+    );
+  }
 }
